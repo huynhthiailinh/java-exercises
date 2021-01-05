f3679ea512564868828d52c63f8c68c8bcb49310
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2021-01-05 10:14:57 +0700

    TI-34

--- src/story/HelloWorld.java
+++ src/story/HelloWorld.java
@@ -1,7 +1,14 @@
 package story;
 
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+import java.util.Scanner;
+
+public class HelloWorld 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		System.out.println("Decimal Number: " + number);
+		System.out.println("Binary Number: " + Integer.toBinaryString(number));
 	}
 }
